21b29482503685fed603e2da7627ada086e28944
Kerem Co <47669514+keremco@users.noreply.github.com>
2023-05-29 18:49:38 +0300

    Added "AbstractClassesDemo"

---
+++ AbstractClassesDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿abstract class Kayit
+{
+    public int Id { get; set; }
+
+    public string Guid { get; set; }
+}
+
+class Urun : Kayit
+{ 
+
+    public string Name { get; set; }
+
+
+}
+
+class Musteri : Kayit
+{
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+}
